f3d5ef8e3503d3c54a88ade7c095bdb3660bd7dd
tlthr764 <tlthr764@gmail.com>
2023-09-18 09:36:08 +0900

    project03 & project04 : second commit

---
+++ project_03/project_03/project_03.cpp
@@ -0,0 +1,67 @@
+﻿// 랜덤 숫자 맞추기
+#include <iostream>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	//사용자 변수 정의
+	vector<int> user;
+	int user_num = 0;
+	int user_check[26] = { 0, };
+	//유저 변수 정의
+	vector<int> computer;
+	mt19937_64 com_random(std::time(nullptr));
+	uniform_int_distribution<__int64> com_num(1, 25);
+	int com_check[26] = { 0, };
+
+	//본문 시작
+	cout << "& 랜덤 숫자 맞추기 (1~25까지의 숫자만 입력이 가능합니다) &" << endl;
+	//숫자 입력 while문
+	int i= 0; // 루프 탈출 변수 정의
+	while (i<6) {
+			cout << i + 1 << " 번째 번호를 입력하세요 : ";
+			cin >> user_num; //숫자 입력
+
+			// 범위 판별 if문
+			if ((user_num > 0 && user_num < 26) && (user_check[user_num] == 0)) {
+				user.push_back(user_num);
+				user_check[user_num] = 1;//사용자 벡터에 숫자 입력
+				i++;
+			}
+			else if (user_check[user_num] == 1) {
+				cout << "중복된 숫자입니다! 새로운 숫자를 입력해주세요." << endl;
+			}
+			else  {
+				cout << "잘못된 입력입니다! 1~25까지의 숫자를 입력하세요." << endl;
+			}
+	}
+
+	// 컴퓨터 숫자 입력 for문 
+	int cc = 0;
+	for (int i = 0; i < 6; i++) {
+		do {
+			cc = com_num(com_random); 
+		} while (com_check[cc] == 1); // do while문
+		computer.insert(computer.begin() + i, cc);
+		com_check[cc] = 1; //컴퓨터 벡터에 숫자 입력
+	}
+
+	// 당첨번호 공개 for문
+	cout << "당첨번호 공개!" << endl;
+	for (int i = 0; i < 6; i++) {
+		cout << computer.at(i) << " ";
+	}
+	cout << endl;
+	cout << "당신의 등수는 과연 몇등일까요?" << endl << "--------------------------------" << endl;
+
+	// 배열 비교 for문
+	int count = 7;
+	for (int i : user){
+		if (user_check[i] == com_check[i]) {
+			count--;
+			}
+	}
+	cout << " → " << count << " 등 입니다!";
+}
---
+++ project_04/project_04/project_04.cpp
@@ -0,0 +1,103 @@
+﻿// 마방진 만들기
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	// 숫자 num 값 입력 받기
+	int num = 0;
+	cout << "홀수인 N으로 마방진 NxN을 만듭니다..." << endl;
+	while (1) {
+		cout << "자연수이자 홀수인 숫자를 입력하세요 : ";
+		cin >> num;
+		if (num > 0 && num % 2 == 1) {
+			break;
+		}
+	}
+	cout << endl;
+
+	// 동적 배열 선언 & 할당
+	int** arr = new int* [num];
+	for (int i = 0; i < num; i++) {
+		arr[i] = new int[num];
+	}
+
+	// 초기 배열 선언
+	for (int i = 0; i < num; i++) {
+		for (int j = 0; j < num; j++) {
+			arr[i][j] = 0; // 동적 배열 초기값 0
+		}
+	}
+
+	// 원소 1의 위치
+	int x = 0;
+	int y = num / 2;
+	int nn = num * num;
+	arr[x][y] = 1;
+
+	// 마방진 본문
+	for (int i = 2; i < nn+1; i++) {
+		int max = num - 1;
+
+		// case1) 원소가 (min,max) 에 위치
+		if ((x - 1) < 0 && (y + 1) > max) {
+			x += 1;
+		}
+
+		// case2) 원소가 (min,y) 에 위치
+		else if ((x - 1) < 0 && (y + 1) <= max) {
+			if (arr[max][y + 1] == 0) {
+				// 이동할 칸이 비어있을 때
+				x = max;
+				y += 1;
+			}
+			else {
+				// 이동할 칸이 이미 채워져 있을 때
+				x += 1;
+			}
+		}
+
+		// case3) 원소가 (x,max) 에 위치
+		else if ((x - 1) >= 0 && (y + 1) > max) {
+			if (arr[x - 1][0] == 0) {
+				// 이동할 칸이 비어있을 때
+				x -= 1;
+				y = 0;
+			}
+			else {
+				// 이동할 칸이 이미 채워져 있을 때
+				x += 1;
+			}
+		}
+		// case3) 원소가 (x,y) 에 위치
+		else {
+			if (arr[x - 1][y + 1] == 0) {
+				// 이동할 칸이 비어있을 때
+				x -= 1;
+				y += 1;
+			}
+			else {
+				// 이동할 칸이 이미 채워져 있을 때
+				x += 1;
+			}
+		}
+		// 배열에 i값 삽입
+		arr[x][y] = i;
+	}
+
+	// 마방진 출력
+	for (int i = 0; i < num; i++) {
+		for (int j = 0; j < num; j++) {
+			cout << setw(3) << arr[i][j];
+		}
+		cout << endl;
+	}
+	
+	
+	// 동적 배열 해제 (반납)
+	for (int i = 0; i < num; i++) {
+		delete[] arr[i];
+	}
+	delete[] arr;
+}
